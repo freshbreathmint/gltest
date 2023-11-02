6a05a16403108d4a991da8d5520434a9cc2f7a03
mints <breathmints@protonmail.com>
2023-11-01 20:56:17 -0700

    display window

--- main.c
+++ main.c
@@ -1,8 +1,71 @@
-#include <glad/glad.h>
+#include "glad.h"
 #include <GLFW/glfw3.h>
 
+#include <stdio.h>
+
+// Settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Process Input
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+}
+
+// Window Size Callback
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    //Make sure viewport is resized when the window size is updated.
+    glViewport(0, 0, width, height);
+}
+
+// Main
 int main() 
 {
-    // starting from scratch
+    // Initalize and Configure GLFW
+    // (Version 4.6, Core Profile)
+    if (!glfwInit()) {
+        printf("Failed to initalize GLFW.\n");
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create and Configure window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "gltest", NULL, NULL);
+    if (!window) {
+        printf("Failed to create window.\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Initalize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initalize GLAD.\n");
+        return 1;
+    } 
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // Process Input
+        processInput(window);
+
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Swap Buffers, Poll Events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Exit Gracefully
+    printf("Exiting...\n");
+    glfwTerminate();
     return 0;
 }
